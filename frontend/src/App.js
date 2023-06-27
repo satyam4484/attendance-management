d21db871c56ed5b5e73d50d7f18c2ce3ffb27aeb
jacktherock <sonawaneabhijeet273@gmail.com>
2023-06-27 11:48:51 +0530

    changes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 const App = () => {
     return (
-        <h1>hello</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quibusdam mollitia iste velit nihil est dolore excepturi laboriosam repellat ea quae, ab molestiae, dolorum dolor quos rem eaque commodi. Excepturi.</h1>
     )
 };
 
