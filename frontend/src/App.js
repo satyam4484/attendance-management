bc3bbe9c0c47f202c730804d44af7e3a81cd9807
satyam <satyambsingh93@gmail.com>
2023-07-31 19:34:52 +0530

    app js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,9 @@ import './assets/css/fonts.css'
 import { useLocation } from "react-router-dom";
 import Navbar from "./Components/UI/Navbar";
 import Routing from "./Routing/Routing";
-<<<<<<< HEAD
-=======
-import {email} from "./network/agent";
-
->>>>>>> f309f99a0877f8fc60e3b4ae04f168c310bfb5f4
 
 const App = () => {
     const location = useLocation();
-    console.log("app");
-    email().then(response => console.log(response))
     
     const isSignUpOrSignIn = () => {
         return location.pathname.includes("/auth/signup") || location.pathname.includes("/auth/signin");
