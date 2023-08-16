c8d7bea1559ebe8dd26b23c19c9b75709b2e6f7a
satyam <satyambsingh93@gmail.com>
2023-08-16 14:11:50 +0530

    profiles added

--- frontend/src/Components/DashBoard/Admin/Admin.js
+++ frontend/src/Components/DashBoard/Admin/Admin.js
@@ -1,3 +1,13 @@
 // list of colleges pending for verification
 // list of 
 // Column
+
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default Admin
